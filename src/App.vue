650f78053cbab76c9d528e011fec024822ff2907
AlexVipond <alex.r.vipond@gmail.com>
2020-12-08 00:37:47 -0600

    reproduces error

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div
+      v-for="(div, index) in divs.values"
+      :key="index"
+      :ref="divs.ref"
+    />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+const values = [0, 1, 2]
+
+export default {
+  setup () {
+    const divEls = ref([]),
+          divs = {
+            ref: el => (divEls.value = [...divEls.value, el]),
+            values,
+          }
+
+    return { divs }
+  }
+}
+</script>
